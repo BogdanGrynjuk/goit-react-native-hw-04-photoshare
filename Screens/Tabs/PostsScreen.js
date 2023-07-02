cc5b33d61ca0d5f933924b66d761e12eeac7e316
BogdanGrynjuk <bgrynjuk@ukr.net>
2023-07-03 02:59:19 +0300

    Adds markup and styles PostsScreen

--- Screens/Tabs/PostsScreen.js
+++ Screens/Tabs/PostsScreen.js
@@ -1,19 +1,58 @@
-import { View, Text, StyleSheet } from "react-native";
-
+import React from "react";
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function PostsScreen() {
   return (
     <View style={styles.container}>
-      <Text>PostsScreen</Text>
+      <View style={styles.header}>
+        <View style={styles.wrapPhoto}></View>
+        <View>
+          <Text style={styles.textName}>Natali Romanova</Text>
+          <Text style={styles.textEmail}>email@example.com</Text>
+        </View>
+      </View>
     </View>
-    
-  )
-};
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    backgroundColor: "#ffffff",
+    paddingHorizontal: 16,
+  },
+
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: 32,
+  },
+
+  wrapPhoto: {
+    marginRight: 8,
+    width: 60,
+    height: 60,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+  },
+
+  textName: {
+    fontFamily: "Roboto-Bold",
+    fontWeight: "bold",
+    fontSize: 14,
+    lineHeight: 15,
+    textAlign: "center",
+    color: "#212121",
+  },
+
+  textEmail: {
+    fontFamily: "Roboto-Regular",
+    fontWeight: "400",
+    fontSize: 12,
+    lineHeight: 13,
+    textAlign: "center",
+    color: "rgba(33, 33, 33, 0.8)",
+  },
+});
