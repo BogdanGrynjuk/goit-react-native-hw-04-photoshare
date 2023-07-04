6076a1110b108a4d56876a9a199c01b113f64834
BogdanGrynjuk <bgrynjuk@ukr.net>
2023-07-04 23:58:36 +0300

    Adds markup and styles CreatePostsScreen

--- Screens/Tabs/ProfileScreen.js
+++ Screens/Tabs/ProfileScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text>This is ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
     </View>
     
   )
